d6254f05546bf2d0e50583d0bd9f0ea3e03f6a28
POGO Vishal <95016059+VishalPaudel@users.noreply.github.com>
2022-10-19 12:54:39 +0530

    * PONG 1

--- my-work/pong/main.lua
+++ my-work/pong/main.lua
@@ -1,20 +1,53 @@
 
+push = require 'push'  --[[ A library ]]
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRT_WIDTH = 432
+VIRT_HEIGHT = 243
+
 function love.load()
-    love.window.setMode( WINDOW_WIDTH, WINDOW_HEIGHT, {
+    --[[    love.window.setMode( WINDOW_WIDTH, WINDOW_HEIGHT, {
+            fullscreen = false,
+            resizable = false,
+            vsync = true
+        })
+    ]]
+
+    love.graphics.setDefaultFilter( 'nearest', 'nearest' )
+
+    push:setupScreen( VIRT_WIDTH, VIRT_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
-    })
+    } )
+end
+
+function love.keypressed()
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
 
 function love.draw()
-    love.graphics.printf(
+    --[[ love.graphics.printf(
         'Hello Vishal!',
         0,
         WINDOW_HEIGHT / 2,
         WINDOW_WIDTH,
         'center')
+    ]]
+    push:apply( 'start' )
+
+    love.graphics.printf(
+        'Hello Pixelated Filter',
+        0,
+        VIRT_HEIGHT / 2,
+        VIRT_WIDTH,
+        'center'
+    )
+    
+    push:apply( 'end' )
 end
+
